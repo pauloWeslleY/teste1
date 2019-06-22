5b711e0b73c579b9b51246c8b44847a4fc8a1417
Weslley Lima <weslleyspfc13@outlook.com>
2019-06-22 00:26:14 -0300

    second

---
+++ index.py
@@ -0,0 +1,19 @@
+nome = 'Barry'
+sobrenome = 'Allen'
+nascimento = 1998
+ano_atual = 2019
+aniversario_ano_atual = false
+
+idade = ano_atual - nascimento if aniversario_ano_atual else(ano_atual - 1 - nascimento)
+
+def function(parameter_list);
+   pass
+
+lista = [['Pro', 'fes', 'sor'], 'da', 'byLearn', ':)']
+
+if idade >= 18:
+   dirigir = True
+else:
+   dirigir = False
+
+print(idade)
